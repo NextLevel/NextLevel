54e0061312f4e09d5e1a5b184ebcacf21a0baa6d
patrick piemonte <patrick.piemonte@gmail.com>
2017-01-22 14:29:12 -0800

    Sample project, remove force unwraps

--- Project/NextLevel/AppDelegate.swift
+++ Project/NextLevel/AppDelegate.swift
@@ -33,12 +33,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame:UIScreen.main.bounds)
-        self.window!.backgroundColor = UIColor.black
+        self.window?.backgroundColor = UIColor.black
         
         let viewController = CameraViewController()
         
-        self.window!.rootViewController = viewController
-        self.window!.makeKeyAndVisible()
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
