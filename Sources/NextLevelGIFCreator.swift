0a009778b9d37352a059bbdf884f82955b27bb84
patrick piemonte <patrick.piemonte@gmail.com>
2019-04-27 22:40:56 -0700

    add GIF creation support

---
+++ Sources/NextLevelGIFCreator.swift
@@ -0,0 +1,91 @@
+//
+//  NextLevelGIFCreator.swift
+//  NextLevel (http://nextlevel.engineering/)
+//
+//  Copyright (c) 2016-present patrick piemonte (http://patrickpiemonte.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+import ImageIO
+import MobileCoreServices
+
+private let NextLevelGIFCreatorQueueIdentifier = "engineering.NextLevel.GIF"
+
+public class NextLevelGIFCreator {
+    
+    // MARK: - ivars
+
+    fileprivate var _queue: DispatchQueue = DispatchQueue(label: NextLevelGIFCreatorQueueIdentifier, attributes: .concurrent)
+    fileprivate var _outputFilePath: URL?
+    
+    fileprivate func createOutputFilePath() -> URL? {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        if let documentsDirectory = paths.first {
+            let outputFilePath = "\(documentsDirectory)/\(Date().iso8601())-NL.gif"
+            return URL(fileURLWithPath: outputFilePath, isDirectory: false)
+        }
+        return nil
+    }
+    
+    // MARK: - factory
+    
+    public func create(gifWithImages images: [UIImage], delay: Float, loopCount: Int = 0, completionHandler: ((_ completed: Bool, _ gifPath: URL?) -> Void)? = nil) {
+        guard let outputFilePath = self.createOutputFilePath() else {
+            DispatchQueue.main.async {
+                completionHandler?(false, nil)
+            }
+            return
+        }
+        
+        self._outputFilePath = outputFilePath
+        self._queue.async {
+            guard let destination = CGImageDestinationCreateWithURL(outputFilePath as CFURL, kUTTypeGIF, images.count, nil) else {
+                DispatchQueue.main.async {
+                    completionHandler?(false, nil)
+                }
+                return
+            }
+
+            let gifProperties: CFDictionary = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: loopCount]] as CFDictionary
+            let frameProperties: CFDictionary = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: delay]] as CFDictionary
+
+            CGImageDestinationSetProperties(destination, gifProperties)
+            for image in images {
+                if let cgImage = image.cgImage {
+                    CGImageDestinationAddImage(destination, cgImage, frameProperties)
+                }
+            }
+            
+            if CGImageDestinationFinalize(destination) {
+                DispatchQueue.main.async {
+                    completionHandler?(true, self._outputFilePath)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completionHandler?(false, nil)
+                }
+            }
+        }
+    }
+    
+}
