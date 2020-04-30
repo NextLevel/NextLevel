962c4c5270475f7f0568bdccb1664a1c238cafcb
GottaGetSwifty <PJ.fechner@gmail.com>
2020-04-30 12:44:40 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 //
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
