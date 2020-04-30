905e2f972394de112ab60bdd4fb1cfa6ebaac002
GottaGetSwifty <PJ.fechner@gmail.com>
2020-04-30 13:01:43 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.2
 //
 //  Package.swift
 //  NextLevel (http://nextlevel.engineering/)
@@ -22,7 +23,6 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 //
-// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
